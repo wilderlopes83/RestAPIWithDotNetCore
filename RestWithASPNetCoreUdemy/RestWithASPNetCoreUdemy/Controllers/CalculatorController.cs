b6920298c9d8035751ab16a33a4219fefe1a33e2
Wilder Lopes <wilderlopes83@gmail.com>
2018-11-16 15:31:37 -0200

    exercicio 1 - calculadora (soma)

---
+++ RestWithASPNetCoreUdemy/RestWithASPNetCoreUdemy/Controllers/CalculatorController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestWithASPNetCoreUdemy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+    
+        // GET api/Sum/5/5
+        [HttpGet("{firstNumber}/{secondNumber}")]
+        public ActionResult<string> Sum(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = Convert.ToDecimal(firstNumber) + Convert.ToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Invalid Input");
+        }        
+
+        private bool IsNumeric(string value)
+        {
+            decimal number;
+
+            bool isNumber = decimal.TryParse(value, System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out number);
+            return isNumber;
+
+        }
+    }
+}
