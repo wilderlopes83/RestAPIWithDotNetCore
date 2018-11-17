a37ca7b1d427ac8c38769d88b24fc25782b2153e
Wilder Lopes <wilderlopes83@gmail.com>
2018-11-17 13:26:02 -0200

    Implementing person controller

---
+++ RestWithASPNetCoreUdemy/RestWithASPNetCoreUdemy/Controllers/PersonController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestWithASPNetCoreUdemy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        // GET api/Person/1
+        [HttpGet("{id}")]
+        public ActionResult<string> Sum(string firstNumber, string secondNumber)
+        {
+            //
+        }
+    }
+}
